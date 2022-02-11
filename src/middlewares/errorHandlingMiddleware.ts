f0388bcf1d129865c4f94c5c444fdfc6ffe47ee0
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2022-02-11 19:49:13 -0300

    fix: delete test code line from last commit

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -4,9 +4,7 @@ import httpStatus from "http-status";
 
 export default function errorHandlingMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
   if (err instanceof ExternalApiError) {
-    return res.status(httpStatus.SERVICE_UNAVAILABLE).send({
-      message: "teste"
-    });
+    return res.status(httpStatus.SERVICE_UNAVAILABLE).send(err.message);
   }
 
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
